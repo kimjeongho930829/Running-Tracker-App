1f52bcbc492a8ee1b0b6a82f8b6eb3decf677c3e
kimjeongho930829 <kimjeongho930829@gmail.com>
2026-05-07 17:21:47 +0900

    실시간 이동 경로 지도 시각화 구현

--- app/src/naver/java/com/jhkim/runningtracker/presentation/component/NaverMapRenderer.kt
+++ app/src/naver/java/com/jhkim/runningtracker/presentation/component/NaverMapRenderer.kt
@@ -1,8 +1,10 @@
 package com.jhkim.runningtracker.presentation.component
 
+import android.graphics.Color
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -13,9 +15,13 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
-import com.jhkim.runningtracker.domain.location.LocationPoint
+import com.jhkim.runningtracker.domain.model.LocationPoint
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraAnimation
+import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
+import com.naver.maps.map.overlay.ArrowheadPathOverlay
 
 class NaverMapRenderer: MapRenderer {
     @Composable
@@ -24,12 +30,45 @@ class NaverMapRenderer: MapRenderer {
         val lifecycleOwner = LocalLifecycleOwner.current
 
         var naverMap by remember { mutableStateOf<NaverMap?>(null) }
+        val arrowOverlay = remember { ArrowheadPathOverlay() }
+
         val mapView = remember {
             MapView(context).apply {
                 getMapAsync {
                     naverMap = it
                 }
             }
+        }
+
+        // Draw Line
+        LaunchedEffect(pathPoints, naverMap) {
+            val map = naverMap ?: return@LaunchedEffect
+
+            if (pathPoints.size >= 2) {
+                arrowOverlay.coords = pathPoints.map {
+                    LatLng(it.latitude, it.longitude)
+                }
+                arrowOverlay.color = Color.BLUE
+                arrowOverlay.outlineWidth = 2
+                arrowOverlay.map = map
+            } else {
+                arrowOverlay.map = null
+            }
+        }
+
+        // Camera Update
+        LaunchedEffect(pathPoints, naverMap) {
+            val lastPoint = pathPoints.lastOrNull() ?: return@LaunchedEffect
+            val map = naverMap ?: return@LaunchedEffect
+
+            val cameraUpdate = CameraUpdate.scrollTo(
+                LatLng(
+                    lastPoint.latitude,
+                    lastPoint.longitude,
+                )
+            ).animate(CameraAnimation.Easing)
+
+            map.moveCamera(cameraUpdate)
         }
 
         AndroidView(
